47842c5bf6f732cc94c8bf93234fced0b10257aa
Bram van den Klinkenberg <bramvandenklinkenberg@gmail.com>
2023-03-06 11:50:45 +0100

    Fix data source reference

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,8 +4,13 @@ resource "azurerm_data_factory" "data_project_adf" {
     resource_group_name = var.resource_group_name
 }
 
+data "azurerm_storage_account" "humidity_data" {
+    name                = var.storage_account_humiditydata_name
+    resource_group_name = var.resource_group_name
+}
+
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "humidity_data" {
     name = var.storage_account_humiditydata_container_name
     data_factory_id = azurerm_data_factory.data_project_adf.id
-    connection_string = output.storage_connection_string
+    connection_string = data.azurerm_storage_account.humidity_data.primary_connection_string
 }
--- infra/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_storage_account" "humidity_data" {
-    name                = var.storage_account_humiditydata_name
-    resource_group_name = var.resource_group_name
-
-}
-
-output "storage_connection_string" {
-    value = "${data.azurerm_storage_account.humidity_data.primary_connection_string}"
-}
