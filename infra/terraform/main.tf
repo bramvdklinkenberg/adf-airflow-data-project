eebd9498b7c9610bc987ee893382d30a2eaf5704
Bram van den Klinkenberg <bramvandenklinkenberg@gmail.com>
2023-03-06 18:12:12 +0100

    Add postgresql adf linked service

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,11 +1,11 @@
-resource "azurerm_data_factory" "data_project_adf" {
-    name                = var.project_name
-    location            = var.location
+data "azurerm_storage_account" "humidity_data" {
+    name                = var.storage_account_humiditydata_name
     resource_group_name = var.resource_group_name
 }
 
-data "azurerm_storage_account" "humidity_data" {
-    name                = var.storage_account_humiditydata_name
+resource "azurerm_data_factory" "data_project_adf" {
+    name                = "${var.project_name}-adf"
+    location            = var.location
     resource_group_name = var.resource_group_name
 }
 
@@ -13,4 +13,10 @@ resource "azurerm_data_factory_linked_service_azure_blob_storage" "humidity_data
     name = var.storage_account_humiditydata_container_name
     data_factory_id = azurerm_data_factory.data_project_adf.id
     connection_string = data.azurerm_storage_account.humidity_data.primary_connection_string
+}
+
+resource "azurerm_data_factory_linked_service_postgresql" "weather_data" {
+    name              = data.azurerm_postgresql_server.data_project_db.name
+    data_factory_id   = azurerm_data_factory.data_project_adf.id
+    connection_string = var.postgresql_connection_string
 }
